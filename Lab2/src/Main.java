939309de5fbcaef9fe42a72b8979431f33171298
Alperen Akgul <alparabellum23@gmail.com>
2022-10-14 14:58:16 +0300

    OOP

---
+++ Lab2/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker worker1 = new Worker();
+        worker1.socialSecurityNumber = 186426;
+        worker1.workingHours = 8;
+        worker1.name = "Alperen";
+        worker1.wage = 12;
+
+        worker1.displayInfo();
+        worker1.displaySalary();
+
+        Worker worker2 = new Worker();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nEnter Worker Name: ");
+        worker2.name = scan.nextLine();
+        System.out.println("\nEnter Worker Wage: ");
+        worker2.wage = scan.nextFloat();
+        System.out.println("\nEnter Worker Working Hours: ");
+        worker2.workingHours = scan.nextInt();
+        System.out.println("\nEnter Worker Social Security Number: ");
+        worker2.socialSecurityNumber = scan.nextInt();
+
+        worker2.displayInfo();
+        worker2.displaySalary();
+
+    }
+}
---
+++ Lab2/src/Worker.java
@@ -0,0 +1,16 @@
+public class Worker {
+    String name;
+    int socialSecurityNumber;
+    float wage;
+    int workingHours;
+
+    void displayInfo(){
+        System.out.printf("Name: %s \nSocial Security Number: %d\n", name, socialSecurityNumber);
+    }
+
+    void displaySalary(){
+        float salary;
+        salary = wage * workingHours;
+        System.out.printf("Salary: %f", salary);
+    }
+}
